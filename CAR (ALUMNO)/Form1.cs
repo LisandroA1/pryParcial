5a10358b717b6c951525dcc1bee99cf6dac29130
Lisandro Arroyo <92538409+LisandroA1@users.noreply.github.com>
2023-06-27 12:20:17 -0300

    Add files via upload

---
+++ CAR (ALUMNO)/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        clsVendedores objVendedores = new clsVendedores();
+        clsVentas objVentas = new clsVentas();
+
+        private void cmdGraficar_Click(object sender, EventArgs e)
+        {
+            DataTable tvendedores = objVendedores.Get_Tabla();
+            DataTable tVentas = objVentas.Get_Tabla();
+            chart1.Series.Clear();
+
+            int desde = Convert.ToInt32(txtDesde.Text);
+            int hasta = Convert.ToInt32(txtHasta.Text);
+
+            for(int aa=desde;aa<=hasta;aa++)
+            {
+               Series serie = chart1.Series.Add(aa.ToString());
+                foreach (ListViewItem a in lsvVendedores.CheckedItems)
+                {
+                    foreach (DataRow fv in tVentas.Rows)
+                    {
+                        serie.Points.AddXY(fv["vendedor"], fv["cantidad"]);
+
+                    }
+
+
+                    
+                }
+
+            }
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            DataTable tv = objVendedores.Get_Tabla();
+
+            foreach(DataRow fv in tv.Rows)
+            {
+                ListViewItem a = lsvVendedores.Items.Add(fv["nombre"].ToString());
+                a.Tag = fv["vendedor"];
+            }
+        }
+    }
+}
---
+++ CAR (ALUMNO)/clsVendedores.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace CAR
+{
+    class clsVendedores
+    {
+        private OleDbConnection conector;
+        private OleDbCommand comando;
+        private OleDbDataAdapter adaptador;
+        private DataTable tabla;
+
+        public clsVendedores()
+        {
+            conector = new OleDbConnection(Properties.Settings.Default.CADENA);
+            comando = new OleDbCommand();
+
+            comando.Connection = conector;
+            comando.CommandType = CommandType.TableDirect;
+            comando.CommandText = "Vendedores";
+
+            adaptador = new OleDbDataAdapter(comando);
+            tabla = new DataTable();
+            adaptador.Fill(tabla);
+
+            DataColumn[] dc = new DataColumn[1];
+            dc[0] = tabla.Columns["Vendedor"];
+            tabla.PrimaryKey = dc;
+        }
+
+        public DataTable Get_Tabla()
+        {
+            return tabla;
+        }
+
+    }
+}
